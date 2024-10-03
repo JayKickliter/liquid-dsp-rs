ef0bc60d468e4bdedc7d5625d224e4d143f2890f
Jay Kickliter <jay@kickliter.com>
2024-10-03 14:59:47 -0600

    [filter/resamp] impl Clone

--- liquid-dsp/src/filter/resamp.rs
+++ liquid-dsp/src/filter/resamp.rs
@@ -4,6 +4,7 @@ use std::{ffi::c_void, marker::PhantomData};
 pub struct Resamp<O, H, S> {
     q: *mut c_void,
     rate: f32,
+    clone_fn: fn(&Resamp<O, H, S>) -> Self,
     drop_fn: fn(&mut Resamp<O, H, S>),
     _output_type: PhantomData<O>,
     _tap_type: PhantomData<H>,
@@ -16,6 +17,12 @@ impl<O, H, S> Drop for Resamp<O, H, S> {
     }
 }
 
+impl<O, H, S> Clone for Resamp<O, H, S> {
+    fn clone(&self) -> Self {
+        (self.clone_fn)(self)
+    }
+}
+
 macro_rules! impl_resamp(
     (
         mod_: $mod:ident,
@@ -25,6 +32,7 @@ macro_rules! impl_resamp(
         input: $S:ty,
         cobj: $Q:path,
         complex_conv: $complex_conv:path,
+        copy_fn: $copy_fn:path,
         create_default_fn: $create_default_fn:path,
         create_fn: $create_fn:path,
         destroy_fn: $destroy_fn:path,
@@ -49,13 +57,27 @@ macro_rules! impl_resamp(
                     }
                 }
 
+                fn clone_fnn(&self) -> Self {
+                    let q = unsafe {$copy_fn(self.q as $Q) as *mut c_void};
+                    Self {
+                        q,
+                        rate: self.rate,
+                        drop_fn: self.drop_fn,
+                        clone_fn: self.clone_fn,
+                        _output_type: PhantomData,
+                        _tap_type: PhantomData,
+                        _sample_type: PhantomData,
+                    }
+                }
+
                 #[doc = concat!("See [resamp_", stringify!($mod), "_create_default](https://liquidsdr.org/api/resamp_", stringify!($mod), "/#create_default).")]
-                pub fn create_default(rate: f32) -> Result<Resamp<$O, $H, $S>, String> {
+                pub fn create_default(rate: f32) -> Result<Self, String> {
                     let q = unsafe { $create_default_fn(rate) as *mut c_void };
                     if q.is_null() {
                         return Err("error".into());
                     }
                     let drop_fn = Self::drop_fn;
+                    let clone_fn = Self::clone_fnn;
                     let _sample_type = PhantomData;
                     let _tap_type = PhantomData;
                     let _output_type = PhantomData;
@@ -64,6 +86,7 @@ macro_rules! impl_resamp(
                         q,
                         rate,
                         drop_fn,
+                        clone_fn,
                         _sample_type,
                         _tap_type,
                         _output_type,
@@ -77,11 +100,12 @@ macro_rules! impl_resamp(
                     fc: f32,
                     as_: f32,
                     npfb: u32,
-                ) -> Result<Resamp<$O, $H, $S>, String> {
+                ) -> Result<Self, String> {
                     let q = unsafe { $create_fn(rate, m, fc, as_, npfb) as *mut c_void };
                     if q.is_null() {
                         return Err("error".into());
                     }
+                    let clone_fn = Self::clone_fnn;
                     let drop_fn = Self::drop_fn;
                     let _sample_type = PhantomData;
                     let _tap_type = PhantomData;
@@ -90,6 +114,7 @@ macro_rules! impl_resamp(
                     Ok(Self {
                         q,
                         rate,
+                        clone_fn,
                         drop_fn,
                         _sample_type,
                         _tap_type,
@@ -133,6 +158,7 @@ impl_resamp!(
     input: Complex32,
     cobj: sys::resamp_cccf,
     complex_conv: crate::complex::c,
+    copy_fn: sys::resamp_cccf_copy,
     create_default_fn: sys::resamp_cccf_create_default,
     create_fn: sys::resamp_cccf_create,
     destroy_fn: sys::resamp_cccf_destroy,
@@ -147,6 +173,7 @@ impl_resamp!(
     input: Complex32,
     cobj: sys::resamp_crcf,
     complex_conv: crate::complex::c,
+    copy_fn: sys::resamp_crcf_copy,
     create_default_fn: sys::resamp_crcf_create_default,
     create_fn: sys::resamp_crcf_create,
     destroy_fn: sys::resamp_crcf_destroy,
@@ -161,6 +188,7 @@ impl_resamp!(
     input: f32,
     cobj: sys::resamp_rrrf,
     complex_conv: crate::complex::ident,
+    copy_fn: sys::resamp_rrrf_copy,
     create_default_fn: sys::resamp_rrrf_create_default,
     create_fn: sys::resamp_rrrf_create,
     destroy_fn: sys::resamp_rrrf_destroy,
@@ -174,46 +202,64 @@ mod tests {
     #[test]
     fn resamp_rrrf() {
         let rate = std::f32::consts::PI;
-        let x: Vec<f32> = (0..100).map(|x| (x as f32).sin()).collect();
+        let x: Vec<f32> = (0..101).map(|x| (x as f32).sin()).collect();
         let mut resamp = ResampRRR::create_default(rate).unwrap();
         let mut y: Vec<f32> = Vec::new();
-        for &xx in x.iter() {
+        for &xx in x[0..100].iter() {
             let mut yy = [0.0; 4];
             let n = resamp.execute(xx, &mut yy).unwrap();
             y.extend(&yy[..n]);
         }
         assert_eq!(y.len(), 315);
+        let mut resamp_clone = resamp.clone();
+        let mut y = [0.0; 4];
+        let mut y_clone = [0.0; 4];
+        resamp.execute(x[100], &mut y).unwrap();
+        resamp_clone.execute(x[100], &mut y_clone).unwrap();
+        assert_eq!(y, y_clone);
     }
 
     #[test]
     fn resamp_crcf() {
         let rate = std::f32::consts::PI;
-        let x: Vec<Complex32> = (0..100)
+        let x: Vec<Complex32> = (0..101)
             .map(|x| (Complex32::from(x as f32)).sin())
             .collect();
         let mut resamp = ResampCRC::create_default(rate).unwrap();
         let mut y: Vec<Complex32> = Vec::new();
-        for &xx in x.iter() {
+        for &xx in x[0..100].iter() {
             let mut yy = [Complex32::from(0.0); 4];
             let n = resamp.execute(xx, &mut yy).unwrap();
             y.extend(&yy[..n]);
         }
         assert_eq!(y.len(), 315);
+        let mut resamp_clone = resamp.clone();
+        let mut y = [Complex32::from(0.0); 4];
+        let mut y_clone = [Complex32::from(0.0); 4];
+        resamp.execute(x[100], &mut y).unwrap();
+        resamp_clone.execute(x[100], &mut y_clone).unwrap();
+        assert_eq!(y, y_clone);
     }
 
     #[test]
     fn resamp_cccf() {
         let rate = std::f32::consts::PI;
-        let x: Vec<Complex32> = (0..100)
+        let x: Vec<Complex32> = (0..101)
             .map(|x| (Complex32::from(x as f32)).sin())
             .collect();
         let mut resamp = ResampCCC::create_default(rate).unwrap();
         let mut y: Vec<Complex32> = Vec::new();
-        for &xx in x.iter() {
+        for &xx in x[0..100].iter() {
             let mut yy = [Complex32::from(0.0); 4];
             let n = resamp.execute(xx, &mut yy).unwrap();
             y.extend(&yy[..n]);
         }
         assert_eq!(y.len(), 315);
+        let mut resamp_clone = resamp.clone();
+        let mut y = [Complex32::from(0.0); 4];
+        let mut y_clone = [Complex32::from(0.0); 4];
+        resamp.execute(x[100], &mut y).unwrap();
+        resamp_clone.execute(x[100], &mut y_clone).unwrap();
+        assert_eq!(y, y_clone);
     }
 }
