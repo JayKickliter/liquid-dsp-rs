0d5037c1686c02da0c8e1c4c998421c57f604f90
Jay Kickliter <jay@kickliter.com>
2024-12-06 14:52:50 -0700

    [benches] add arb resamp benchmarks

--- liquid-dsp/benches/benchmarks.rs
+++ liquid-dsp/benches/benchmarks.rs
@@ -2,16 +2,45 @@ use criterion::criterion_main;
 
 mod filt {
     pub mod resamp {
-        use criterion::Criterion;
-        // use liquid_dsp::filter::resamp::{ResampCCC, ResampCRC, ResampRRR};
-        // use num_complex::Complex32;
-        // use std::hint::black_box;
+        use criterion::{Criterion, Throughput};
+        use liquid_dsp::filter::resamp::{
+            // ResampCCC,
+            ResampCRC,
+            // ResampRRR
+        };
+        use num_complex::Complex32;
 
-        pub fn resamp_ccc(c: &mut Criterion) {}
-        pub fn resamp_CRC(c: &mut Criterion) {}
-        pub fn resamp_rrr(c: &mut Criterion) {}
+        pub fn resamp_ccc(c: &mut Criterion) {
+            let rate = std::f32::consts::PI;
+            let mut resamp = ResampCRC::create_default(rate).unwrap();
+            let xs = vec![Complex32::from(1.0); 1024 * 1024];
+            let mut yy = vec![Complex32::from(0.0); 1024 * 1024 * 4];
 
-        criterion::criterion_group!(resamp, resamp_ccc, resamp_CRC, resamp_rrr);
+            let mut g = c.benchmark_group("ResampCCC single");
+            g.throughput(Throughput::Elements(xs.len() as u64));
+            g.bench_function("execute", |z| {
+                z.iter(|| {
+                    for x in xs.iter() {
+                        resamp.execute(*x, &mut yy).unwrap();
+                    }
+                })
+            });
+            g.finish();
+
+            let mut g = c.benchmark_group("ResampCCC block");
+            g.throughput(Throughput::Elements(xs.len() as u64));
+            g.bench_function("execute_batch", |z| {
+                z.iter(|| {
+                    resamp.execute_block(&xs, &mut yy).unwrap();
+                })
+            });
+            g.finish();
+        }
+
+        pub fn resamp_crc(_c: &mut Criterion) {}
+        pub fn resamp_rrr(_c: &mut Criterion) {}
+
+        criterion::criterion_group!(resamp, resamp_crc, resamp_ccc, resamp_rrr);
     }
 }
 
